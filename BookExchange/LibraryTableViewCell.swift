33000998628bb981c0a77557a384c316afcbc867
zhangyunchen <nail@mrzozo.com>
2016-04-14 21:33:08 +0800

    makes the table view works fine

---
+++ BookExchange/LibraryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LibraryTableViewCell.swift
+//  BookExchange
+//
+//  Created by zhangyunchen on 16/4/14.
+//  Copyright © 2016年 zhangyunchen. All rights reserved.
+//
+
+import UIKit
+
+class LibraryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- BookExchange/LibraryTableViewController.swift
+++ BookExchange/LibraryTableViewController.swift
@@ -9,15 +9,50 @@
 import UIKit
 
 class LibraryTableViewController: UITableViewController {
+    
+    //MARK:Properties
+    let InitIndentifier = "LibraryCell"
+    var cellForHeight:LibraryTableViewCell!
+//    let CellForHeight = self.tableView.dequeueReusableCellWithIdentifier(initIndentifier) as! LibraryTableViewCell
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        cellForHeight = tableView.dequeueReusableCellWithIdentifier(InitIndentifier) as! LibraryTableViewCell
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    //MARK: Table view delegate.
+    
+    // return number of sections in this tableview, and now we just need one.
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // return number of rows in one section, and it will be the hole number of books.
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    //return the estimated height for row
+    override func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 70
+    }
+    
+    // return heigh of one row.
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return cellForHeight.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize).height
+    }
+    
+    // return the cell of onew row
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let initIndentifier = "LibraryCell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(initIndentifier, forIndexPath: indexPath) as! LibraryTableViewCell
+        return cell
     }
 
 
