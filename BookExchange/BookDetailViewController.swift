aefe734070b5443d8b67e8887101b5d0064bb5c0
zhangyunchen <nail@mrzozo.com>
2016-04-30 01:15:37 +0800

    add bookcoverImageView and bookName,bookDescription to book detail view.

--- BookExchange/BookDetailViewController.swift
+++ BookExchange/BookDetailViewController.swift
@@ -11,13 +11,32 @@ import APESuperHUD
 
 class BookDetailViewController: UIViewController {
     
-    //MARK:Properties
+    //MARK: Properties
     var book:Book?
     var bookDetail:BookDetail?
+    @IBOutlet weak var bookCover: UIImageView!
+    @IBOutlet weak var bookName: UILabel!
+    @IBOutlet weak var bookDescription: UILabel!
+    @IBOutlet weak var contentViewWidth: NSLayoutConstraint!
+    @IBOutlet weak var contentViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var subbmitButton: UIButton!
+    
 
+    
+    //MARK: Method
     override func viewDidLoad() {
         super.viewDidLoad()
         title = book?.bookName
+        contentViewWidth.constant = UIScreen.mainScreen().bounds.width
+        subbmitButton.layer.cornerRadius = 2
+        
+        if let url = book?.bookCoverUrl {
+            bookCover.sd_setImageWithURL(NSURL( string: url),placeholderImage:UIImage(named: "placeHolder"))
+        }
+        bookName.text = book?.bookName
+        bookDescription.text = book?.bookDescription
+        
         APESuperHUD.showOrUpdateHUD(loadingIndicator: .Standard, message: "正在获取图书数据", presentingView:view)
         BookExchangeService.getBookDeailWithISBN(book?.bookISBN){
             book in
@@ -27,6 +46,10 @@ class BookDetailViewController: UIViewController {
                 APESuperHUD.removeHUD(animated: true, presentingView: self.view, completion: nil)
             }
         }
+    }
+    
+    @IBAction func didSubbmitPressed(sender: UIButton) {
+        
     }
     
 
